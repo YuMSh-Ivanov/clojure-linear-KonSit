0692c34cf5a4f7aadc58e847763dd86e94afb831
KonSit <konsitness@gmail.com>
2024-11-17 23:48:54 +0300

    cringe

--- src/linear.clj
+++ src/linear.clj
@@ -1,35 +1,57 @@
 (ns linear)
 
 (defn v+ [& vectors]
+  {:pre [(and (not (empty? vectors)) (apply = (map count vectors)) (every? vector? vectors))]}
   (apply mapv + vectors))
 
 (defn v- [& vectors]
+  {:pre [(and (not (empty? vectors)) (apply = (map count vectors)) (every? vector? vectors))]}
   (apply mapv - vectors))
 
 (defn v* [& vectors]
+  {:pre [(and (not (empty? vectors)) (apply = (map count vectors)) (every? vector? vectors))]}
   (apply mapv * vectors))
 
 (defn vd [& vectors]
+  {:pre [(and (not (empty? vectors)) (apply = (map count vectors)) (every? vector? vectors))]}
   (apply mapv / vectors))
 
 (defn dot [& vectors]
+  {:pre [(and (not (empty? vectors)) (apply = (map count vectors)) (every? vector? vectors))]}
   (if (empty? vectors)
     0
     (apply + (apply v* vectors))))
 
 (defn v*s [vector & scalars]
+  {:pre [(and (every? number? scalars) (vector? vector))]}
   (mapv #(* % (apply * scalars)) vector))
 
 (defn m+ [& matrices]
+  {:pre [(and (not (empty? matrices))
+              (apply = (map #(count %) matrices))
+              (apply = (map #(count (first %)) matrices))
+              (reduce #(and %1 %2) (vec (map #(apply = (map count %)) matrices))))]}
   (apply mapv v+ matrices))
 
 (defn m- [& matrices]
+  {:pre [(and (not (empty? matrices))
+              (apply = (map #(count %) matrices))
+              (apply = (map #(count (first %)) matrices))
+              (reduce #(and %1 %2) (vec (map #(apply = (map count %)) matrices))))]}
   (apply mapv v- matrices))
 
 (defn m* [& matrices]
+  {:pre [(and (not (empty? matrices))
+              (apply = (map #(count %) matrices))
+              (apply = (map #(count (first %)) matrices))
+              (reduce #(and %1 %2) (vec (map #(apply = (map count %)) matrices))))]}
   (apply mapv v* matrices))
 
 (defn md [& matrices]
+  {:pre [(and (not (empty? matrices))
+              (apply = (map #(count %) matrices))
+              (apply = (map #(count (first %)) matrices))
+              (reduce #(and %1 %2) (vec (map #(apply = (map count %)) matrices))))]}
   (apply mapv vd matrices))
 
 (defn m*s [matrix & scalars]
@@ -44,4 +66,4 @@
 (defn m*m [& matrices]
   (reduce (fn [cur next] (mapv #(m*v (transpose next) %) cur)) matrices))
 
-(print ((v+ -19.01 -51.14)))
+(print (m+ [1 [1 2]]))
